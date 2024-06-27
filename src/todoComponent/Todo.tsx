e3ed921e92747aa8773992d6e59a46753dbbdcdd
Demostat101 <demostat1995@gmail.com>
2024-06-27 11:23:36 -0700

    add functionalities

--- src/todoComponent/Todo.tsx
+++ src/todoComponent/Todo.tsx
@@ -11,38 +11,30 @@ const Todo = () => {
   const [addTodo, setAddTodo] = useState("");
   const [search, setSearch] = useState("");
   const [todo, setTodo] = useState([]);
-  const [error, setError] = useState(null)
-  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [completed, setCompleted] = useState(0);
+  const [activeTask, setActiveTask] = useState(0);
   const inputRef = useRef();
 
   const restApi = async () => {
     try {
+      const data = await fetch(API_URL);
+      if (!data.ok) {
+        throw new Error("Did not receive the correct data");
+      } else {
+        setIsLoading(true);
+      }
+      const response = await data.json();
+      setTodo(response);
+      setError(null);
 
-        const data = await fetch(API_URL);
-        if (!data.ok) {
-
-            throw new Error("Did not receive the correct data");   
-            
-        } else{
-            setIsLoading(true)
-        }
-        const response = await data.json();
-       setTodo(response)
-       setError(null)
-       
-        return response
-        
-        
-        
+      return response;
     } catch (error) {
-       
-        setError(error.message)
-        setIsLoading(false)
-        
+      setError(error.message);
+      setIsLoading(false);
     }
-  }
-
-  
+  };
 
   const searchHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
@@ -50,7 +42,7 @@ const Todo = () => {
 
   useEffect(() => {
     setTimeout(() => {
-        restApi()
+      restApi();
     }, 3000);
   }, []);
 
@@ -70,17 +62,17 @@ const Todo = () => {
 
       //to add task to database
       const postOption = {
-        method:"POST",
+        method: "POST",
         headers: {
-          "content-type" : "application/json"
+          "content-type": "application/json",
         },
-        body: JSON.stringify(addData)
-      }
+        body: JSON.stringify(addData),
+      };
 
-      const result = await apiRequest(API_URL, postOption)
+      const result = await apiRequest(API_URL, postOption);
 
       if (result) {
-        setError(result)
+        setError(result);
       }
 
       return setTodo((prev) => {
@@ -89,7 +81,31 @@ const Todo = () => {
     }
   };
 
-    //to filter by task in the search input
+  // COMPLETED TASKS
+
+  let arr = [];
+
+  useEffect(() => {
+    todo.map((val) => {
+      val.completed
+        ? setCompleted(arr.push(val.completed))
+        : setCompleted(arr.length);
+    });
+  }, [todo]);
+
+  /// ACTIVE TASK
+  
+  let arr1 = [];
+
+  useEffect(() => {
+    todo.map((val) => {
+      !val.completed
+        ? setActiveTask(arr1.push(!val.completed))
+        : setActiveTask(arr1.length);
+    });
+  }, [todo]);
+
+  //to filter by task in the search input
   const loadSearch = todo.filter(
     (item) => item.task.toLowerCase().indexOf(search.toLowerCase()) !== -1
   );
@@ -98,17 +114,17 @@ const Todo = () => {
     setTodo(todo.filter((item) => item.id !== id));
 
     const deleteOptions = {
-      method : "DELETE",
-      }
+      method: "DELETE",
+    };
 
-    const reqUrl = `${API_URL}/${id}`
-    const result = await apiRequest(reqUrl, deleteOptions)
+    const reqUrl = `${API_URL}/${id}`;
+    const result = await apiRequest(reqUrl, deleteOptions);
     if (result) {
-      setError(result)
+      setError(result);
     }
   };
 
-  const handleCheckBox = async(id: number) => {
+  const handleCheckBox = async (id: number) => {
     const toggleCheckBox = todo.map((item) =>
       item.id === id ? { ...item, completed: !item.completed } : item
     );
@@ -117,29 +133,25 @@ const Todo = () => {
 
     //to update task
 
-    const myItem = toggleCheckBox.filter((item)=> item.id === id)
+    const myItem = toggleCheckBox.filter((item) => item.id === id);
 
     const updateOptions = {
-      method : "PATCH",
-      headers : {
-        "content-type" : "application/json"
+      method: "PATCH",
+      headers: {
+        "content-type": "application/json",
       },
-      body:JSON.stringify({completed: myItem[0].completed})
-    }
+      body: JSON.stringify({ completed: myItem[0].completed }),
+    };
 
-    const reqUrl = `${API_URL}/${id}`
-    const result = await apiRequest(reqUrl, updateOptions)
+    const reqUrl = `${API_URL}/${id}`;
+    const result = await apiRequest(reqUrl, updateOptions);
     if (result) {
-      setError(result)
+      setError(result);
     }
   };
 
-  
-
   return (
     <ul className=" w-96 flex flex-col place-items-left justify-center ml-10 mt-10  h-full">
-
-        
       <TodoInput
         addTodo={addTodo}
         addNewData={addNewData}
@@ -147,37 +159,50 @@ const Todo = () => {
         inputRef={inputRef}
       />
       <Search searchHandler={searchHandler} search={search} />
-      <div className="text-red-700"> {error ?` Error: ${error}`:""}</div>
+      <div className="text-red-700"> {error ? ` Error: ${error}` : ""}</div>
 
-      {!isLoading ? <div className="loader mt-24 ml-10"></div>: ""}
-      {loadSearch.map((item) => {
-        return (
-          <li
-            key={Math.random()}
-            className="flex w-96 justify-between place-items-center "
-          >
-            <div className="flex text-xl font-bold gap-4 h-full pt-3">
-              <input
-                type="checkbox"
-                checked={item.completed}
-                onChange={() => handleCheckBox(item.id)}
-                className="w-8 h-8 mb-3"
+      {!isLoading ? <div className="loader mt-24 ml-10"></div> : ""}
+      <div className="flex flex-col w-full h-full place-items-center justify-center ">
+        {loadSearch.map((item) => {
+          return (
+            <li
+              key={Math.random()}
+              className="flex w-full h-10 justify-center place-items-center m-2"
+            >
+              <div className="flex text-sm font-bold gap-4 w-full h-full pt-3">
+                <input
+                  type="checkbox"
+                  checked={item.completed}
+                  onChange={() => handleCheckBox(item.id)}
+                  className="w-5 h-5 mb-3"
+                />
+                <div>
+                  {!item.completed === true ? (
+                    <div className=" w-full h-full">{item.task}</div>
+                  ) : (
+                    <del
+                      className=" w-full h-full text-center"
+                      style={{ opacity: "0.4" }}
+                    >
+                      {item.task}
+                    </del>
+                  )}
+                </div>
+              </div>
+
+              <FaTrash
+                size={20}
+                role="button"
+                onClick={() => handleDelete(item.id)}
               />
-              {!item.completed === true ? (
-                <div>{item.task}</div>
-              ) : (
-                <del>{item.task}</del>
-              )}
-            </div>
-
-            <FaTrash
-              size={20}
-              role="button"
-              onClick={() => handleDelete(item.id)}
-            />
-          </li>
-        );
-      })}
+            </li>
+          );
+        })}
+      </div>
+      <div>
+        {<div>Active Task: {activeTask}</div>}
+        {<div>Completed Task: {completed}</div>}
+      </div>
     </ul>
   );
 };
